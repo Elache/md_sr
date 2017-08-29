eb2370ed83be0eb61c544ea0db604f2c3f9bb360
Elache <lohibon@gmail.com>
2017-08-29 21:19:30 +0200

    Base v4 : init Categorie

--- app/src/main/java/fr/hibon/modepassesecurest/compte/bdd/GestionBaseLocale.java
+++ app/src/main/java/fr/hibon/modepassesecurest/compte/bdd/GestionBaseLocale.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import fr.hibon.modepassesecurest.compte.bdd.table.Table_Categorie;
-import fr.hibon.modepassesecurest.compte.bdd.table.Table_Compte_Utilisateur;
-import fr.hibon.modepassesecurest.compte.bdd.table.Table_Donnee;
-import fr.hibon.modepassesecurest.compte.bdd.table.Table_Repertoire;
-import fr.hibon.modepassesecurest.compte.bdd.table.Table_Site_Web;
+import static fr.hibon.modepassesecurest.compte.bdd.table.Table_Categorie.*;
+import static fr.hibon.modepassesecurest.compte.bdd.table.Table_Compte_Utilisateur.* ;
+import static fr.hibon.modepassesecurest.compte.bdd.table.Table_Donnee.*;
+import static fr.hibon.modepassesecurest.compte.bdd.table.Table_Repertoire.*;
+import static fr.hibon.modepassesecurest.compte.bdd.table.Table_Site_Web.*;
+import static fr.hibon.modepassesecurest.compte.bdd.table.Table_Categorie.*;
+
+
 
 /**
  * Created by lohib on 20/08/2017.
@@ -17,31 +20,11 @@ import fr.hibon.modepassesecurest.compte.bdd.table.Table_Site_Web;
 public class GestionBaseLocale extends SQLiteOpenHelper {
 
     /* Nom de la base de donnees */
-    public static final String DATABASE_NOM = "modepasse_securest.db";
-    public static final int DATABASE_VERSION = 3;
+    private static final String DATABASE_NOM = "modepasse_securest.db";
+    private static final int DATABASE_VERSION = 4;
 
 
-    /* ** CREATION DES TABLES - REQUETES ********
-    * ****************** */
-
-    /* Table Categorie */
-    public static final String CATEGORIE_TABLE_CREATE = Table_Categorie.CATEGORIE_TABLE_CREATE;
-
-    /* Table Compte_Utilisateur */
-    public static final String COMPTE_USER_TABLE_CREATE = Table_Compte_Utilisateur.COMPTE_USER_TABLE_CREATE;
-
-    /* Table Donnee */
-    public static final String DONNEE_TABLE_CREATE = Table_Donnee.DONNEE_TABLE_CREATE;
-
-    /* Table Site_web */
-    public static final String SITE_WEB_TABLE_CREATE = Table_Site_Web.SITE_WEB_TABLE_CREATE;
-
-    /* Table Repertoire */
-    public static final String REPERTOIRE_TABLE_CREATE = Table_Repertoire.REPERTOIRE_TABLE_CREATE;
-
-
-
-       /* ** METHODES ********
+        /* ** METHODES ********
         * ****************** */
 
 
@@ -60,16 +43,28 @@ public class GestionBaseLocale extends SQLiteOpenHelper {
         db.execSQL(DONNEE_TABLE_CREATE);
         db.execSQL(CATEGORIE_TABLE_CREATE) ;
         db.execSQL(SITE_WEB_TABLE_CREATE);
+        db.execSQL(lesCategories());
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + Table_Donnee.DONNEE_TABLE_NOM + " ; ");
-        db.execSQL("DROP TABLE IF EXISTS " + Table_Repertoire.REPERTOIRE_TABLE_NOM + " ; " );
-        db.execSQL("DROP TABLE IF EXISTS " + Table_Compte_Utilisateur.COMPTE_USER_TABLE_NOM + " ; " );
-        db.execSQL("DROP TABLE IF EXISTS " + Table_Categorie.CATEGORIE_TABLE_NOM + " ; " );
-        db.execSQL("DROP TABLE IF EXISTS " + Table_Site_Web.SITE_WEB_TABLE_NOM + " ;" );
+        db.execSQL("DROP TABLE IF EXISTS " + DONNEE_TABLE_NOM + " ; ");
+        db.execSQL("DROP TABLE IF EXISTS " + REPERTOIRE_TABLE_NOM + " ; " );
+        db.execSQL("DROP TABLE IF EXISTS " + COMPTE_USER_TABLE_NOM + " ; " );
+        db.execSQL("DROP TABLE IF EXISTS " + CATEGORIE_TABLE_NOM + " ; " );
+        db.execSQL("DROP TABLE IF EXISTS " + SITE_WEB_TABLE_NOM + " ;" );
         onCreate(db);
     }
 
+
+    /* ** CATEGORIE : INITIALISATION TABLE * */
+    private static final String lesCategories() {
+        String[] cat = {"Général", "Accès Internet et connexions", "E-mail", "Réseaux sociaux", "Sites web Loisirs", "Jeux", "Services web", "Codes confidentiels", "Autres"};
+        String reqInsertPrefixe = "INSERT INTO " + CATEGORIE_TABLE_NOM + " (" + CATEGORIE_NOM + ") VALUES ('";
+        String reqInsertSuffixe = "') ;";
+        String requete = "";
+        for (int i = 0; i < cat.length; i++)
+            requete += reqInsertPrefixe + cat[i] + reqInsertSuffixe;
+        return requete;
+    }
 }
